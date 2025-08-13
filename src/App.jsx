3f511b65cd6284983337cb9b186c11b310c02950
Bishal Chalise <bishalise@gmail.com>
2025-08-13 15:50:46 +0200

    until router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { list } from "postcss";
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+};
+
+export default App;
