b167dcd131f15b70c48fc85b5c53be706ddfe9cf
Bishal Chalise <bishalise@gmail.com>
2025-08-16 10:35:56 +0200

    job-page-added

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import JobListing from "../components/JobListing";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      const apiUrl = `/api/jobs/${id}`;
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
   return (
     <section>
-      <h1>Boisha</h1>
-      <JobListing />
+      <h1>{job.title}</h1>
     </section>
   );
 };
